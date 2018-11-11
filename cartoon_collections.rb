85b7ded13e3b5353fc62ca4ad1e2f9a5b376e328
Jason Stone <jason.stone0808@gmail.com>
2018-11-11 22:42:17 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,9 @@ def long_planeteer_calls(calls)
   end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(items)
+  cheeses = ["cheddar", "gouda", "camembert"]
+  items.find do |cheese|
+    cheeses.include?(cheese)
+  end
 end
